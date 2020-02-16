6bc59af876d67383703a7df64a2bf47d2a403ae5
Erik <erikadarisman@gmail.com>
2020-02-16 12:24:00 +0700

    add class n insiate button with toast

---
+++ app/src/main/java/com/kadarisman/noteapp/AddNoteController.kt
@@ -0,0 +1,19 @@
+package com.kadarisman.noteapp
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.post_note.*
+
+class AddNoteController : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.post_note)
+
+        val buttonPost = bt_post_note
+        buttonPost.setOnClickListener {
+            Toast.makeText(this,"Post Note!!",Toast.LENGTH_LONG).show()
+        }
+    }
+}
